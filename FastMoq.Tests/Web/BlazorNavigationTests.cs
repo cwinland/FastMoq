d4b6b7691140fb07cab767435fec658111723c56
Christopher Winland <chrislw77@gmail.com>
2022-11-22 17:13:40 -0500

    .

--- FastMoq.Tests/Web/BlazorNavigationTests.cs
+++ FastMoq.Tests/Web/BlazorNavigationTests.cs
@@ -1,14 +1,8 @@
-﻿using AngleSharp.Html.Dom;
-using Bunit;
-using Bunit.TestDoubles;
+﻿using Bunit.TestDoubles;
 using FastMoq.Tests.Blazor.Pages;
-using FastMoq.Tests.Blazor.Shared;
 using FastMoq.Web.Blazor;
 using FluentAssertions;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace FastMoq.Tests.Web
@@ -28,6 +22,7 @@ namespace FastMoq.Tests.Web
 
             IsExists("button").Should().BeTrue();
             ButtonClick("button", () => manager.History.Count > 0);
+            
         }
     }
 }
