f422537ee1ef5c2b9c6d981ee4cd03bbb3cf0592
Chris Winland <chriswin@microsoft.com>
2024-06-12 06:39:24 -0400

    Create observable exception log object

---
+++ FastMoq.Core/Models/ObservableExceptionLog.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace FastMoq.Models
+{
+    public class ObservableExceptionLog : IReadOnlyCollection<string>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        private readonly ObservableCollection<string> internalCollection = new ();
+        private readonly ReadOnlyObservableCollection<string> readOnlyCollection;
+
+        public ObservableExceptionLog()
+        {
+            readOnlyCollection = new ReadOnlyObservableCollection<string>(internalCollection);
+        }
+
+        // Internal method to add an item
+        internal void Add(string item)
+        {
+            internalCollection.Add(item);
+        }
+
+        // Public read-only property
+        public IReadOnlyCollection<string> Items => readOnlyCollection;
+
+        /// <inheritdoc />
+        public event NotifyCollectionChangedEventHandler? CollectionChanged
+        {
+            add => ((INotifyCollectionChanged) readOnlyCollection).CollectionChanged += value;
+            remove => ((INotifyCollectionChanged) readOnlyCollection).CollectionChanged -= value;
+        }
+
+        /// <inheritdoc />
+        public event PropertyChangedEventHandler? PropertyChanged
+        {
+            add => ((INotifyPropertyChanged) readOnlyCollection).PropertyChanged += value;
+            remove => ((INotifyPropertyChanged) readOnlyCollection).PropertyChanged -= value;
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<string> GetEnumerator() => readOnlyCollection.GetEnumerator();
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) readOnlyCollection).GetEnumerator();
+
+        /// <inheritdoc />
+        public int Count => readOnlyCollection.Count;
+    }
+}
